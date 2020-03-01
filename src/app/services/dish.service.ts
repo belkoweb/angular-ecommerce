14e8eb2471779ca62882e1c9f42ba789e5a7664c
belkoweb <belkoweb9718@gmail.com>
2020-03-01 16:14:35 +0100

    RxJs Part 2

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -24,6 +24,9 @@ export class DishService {
    return of(DISHES.filter((dish) => dish.featured)[0]).pipe(delay(2000));
   }
 
+  getDishIds(): Observable<string[] | any> {
+    return of(DISHES.map(dish => dish.id ));
+  }
  /*
   getDishes(): Promise<Dish[]> {
     return of(DISHES).pipe(delay(2000)).toPromise();
